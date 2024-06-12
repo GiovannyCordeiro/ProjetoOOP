1f1dd8f93cdb3d0b3d6be3fbcc783809249f3dc6
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2024-06-12 16:51:09 -0300

    feat: menu da aplicação principal

--- src/App.java
+++ src/App.java
@@ -1,6 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Boa tarde!");
+        ArrayList ListaProdutos = new ArrayList<>();
+        ArrayList ListaUsuarios = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        String opcaoEscolhida = "";
+
+        System.out.println("------------------------------");
+        System.out.println("Bem vindo ao sistema!");
+        System.out.println("Escolha uma das opções abaixo!");
+        
+        System.out.println("1 - Adicionar usuario");
+        System.out.println("2 - Opções para produtos");
+        System.out.println("3 - Registrar vendas");
+        System.out.println("4 - Adicionar oferta de desconto");
+        System.out.println("5 - Gerar relatorios de vendas");
+        System.out.println("6 - Sair da aplicação");
+        System.out.println("------------------------------");
+
+        // Loop para verificação de outras opções incorretas
+        while (!opcaoEscolhida.equalsIgnoreCase("6")) {
+          opcaoEscolhida = scanner.nextLine();
+          switch (opcaoEscolhida) {
+            case "1":
+              System.out.println("ENTRANDO NA LOGICA 1");
+              break;
+            case "2":
+              System.out.println("ENTRANDO NA LOGICA 2");
+              break;
+            case "3":
+              System.out.println("ENTRANDO NA LOGICA 3");
+              break;
+            case "4":
+              System.out.println("ENTRANDO NA LOGICA 4");
+              break;
+            case "5":
+              System.out.println("ENTRANDO NA LOGICA 5");
+              break;
+            case "6":
+              System.out.println("Te vejo em breve!");
+              break;
+            default:
+              System.out.println("Escolha uma das opções sugeridas");
+              break;
+          }
+        }
+        scanner.close();
     }
 }
